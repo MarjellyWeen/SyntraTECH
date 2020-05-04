d0fe45d224023a5b96907dfc99d5e3224bd4e07e
mdenitti <massimodn77@gmail.com>
2020-05-04 21:47:50 +0200

    MYSQL dump update

--- laravel6.sql
+++ laravel6.sql
@@ -16,6 +16,62 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `contacts`
+--
+
+DROP TABLE IF EXISTS `contacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contacts` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `question` text,
+  `created_at` date DEFAULT NULL,
+  `updated_at` date DEFAULT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contacts`
+--
+
+LOCK TABLES `contacts` WRITE;
+/*!40000 ALTER TABLE `contacts` DISABLE KEYS */;
+INSERT INTO `contacts` VALUES (1,'Karel','karel@carolus.com','00909090','Kan ik mij nog inschrijven voor de online lessen?','2020-04-27','2020-04-27'),(2,'Karel','karel@carolus.com','00909090','Kan ik mij nog inschrijven voor de online lessen?','2020-04-27','2020-04-27'),(3,NULL,NULL,NULL,NULL,'2020-04-27','2020-04-27'),(4,'sdfsdfsd','sdfsdf','sdfsdfdf','sdfsdfdf','2020-04-27','2020-04-27'),(5,'Massimo','help@massimo.com','111111111111','Mijn vraag','2020-04-27','2020-04-27');
+/*!40000 ALTER TABLE `contacts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `course_student`
+--
+
+DROP TABLE IF EXISTS `course_student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `course_student` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `course_id` int DEFAULT NULL,
+  `student_id` int DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  `updated_at` date DEFAULT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `course_student`
+--
+
+LOCK TABLES `course_student` WRITE;
+/*!40000 ALTER TABLE `course_student` DISABLE KEYS */;
+INSERT INTO `course_student` VALUES (1,1,1,'2020-04-30','2020-04-30'),(2,1,2,'2020-04-30','2020-04-30'),(3,1,3,'2020-04-30','2020-04-30'),(4,2,1,'2020-04-30','2020-04-30'),(5,2,3,'2020-04-30','2020-04-30'),(6,3,3,'2020-04-30','2020-04-30'),(7,4,2,'2020-04-30','2020-04-30'),(8,4,3,'2020-04-30','2020-04-30'),(12,4,12,NULL,NULL),(13,3,13,NULL,NULL),(17,3,15,NULL,NULL),(18,2,16,NULL,NULL),(29,1,21,NULL,NULL),(30,2,21,NULL,NULL),(31,3,21,NULL,NULL);
+/*!40000 ALTER TABLE `course_student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `courses`
 --
 
@@ -27,10 +83,12 @@ CREATE TABLE `courses` (
   `name` varchar(100) DEFAULT NULL,
   `price` int DEFAULT NULL,
   `description` varchar(250) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
   `full` text,
+  `location_id` int DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  `updated_at` date DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +97,7 @@ CREATE TABLE `courses` (
 
 LOCK TABLES `courses` WRITE;
 /*!40000 ALTER TABLE `courses` DISABLE KEYS */;
-INSERT INTO `courses` VALUES (1,'php',450,'PHP (PHP: Hypertext Preprocessor) is een scripttaal, die bedoeld is om op webservers','2020-04-21','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),(2,'js',350,'Veel Internet websites bevatten JavaScript, een programmeertaal voor scripts die wordt','2020-04-22','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),(3,'python',250,'Python is ontwikkeld met het oog op leesbare code. Hieruit vloeit haar \"zuivere\" stijl voort. ','2020-04-23','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),(4,'MySQL',250,'MySQL is een propriëtair opensource-managementsysteem voor relationele databases (RDBMS). ','2020-04-24','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
+INSERT INTO `courses` VALUES (1,'php',450,'PHP (PHP: Hypertext Preprocessor) is een scripttaal, die bedoeld is om op webservers','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',1,'2020-04-21','2020-04-21'),(2,'js',350,'Veel Internet websites bevatten JavaScript, een programmeertaal voor scripts die wordt','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',1,'2020-04-22','2020-04-21'),(3,'python',250,'Python is ontwikkeld met het oog op leesbare code. Hieruit vloeit haar \"zuivere\" stijl voort. ','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',2,'2020-04-23','2020-04-21'),(4,'MySQL',250,'MySQL is een propriëtair opensource-managementsysteem voor relationele databases (RDBMS). ','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',4,'2020-04-20','2020-04-21');
 /*!40000 ALTER TABLE `courses` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,8 +110,12 @@ DROP TABLE IF EXISTS `locations`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `locations` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `cap` int DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  `updated_at` date DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,6 +124,7 @@ CREATE TABLE `locations` (
 
 LOCK TABLES `locations` WRITE;
 /*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+INSERT INTO `locations` VALUES (1,'T2 Campus',2600,'2020-04-30','2020-04-30'),(2,'Syntra Campus Hasselt',1200,'2020-04-30','2020-04-30'),(3,'Syntra Campus Pelt',1000,'2020-04-30','2020-04-30'),(4,'Syntra Campus Tongeren',800,'2020-04-30','2020-04-30'),(5,'Syntra Campus Westerlo',600,'2020-04-30','2020-04-30');
 /*!40000 ALTER TABLE `locations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -81,7 +144,7 @@ CREATE TABLE `students` (
   `updated_at` date DEFAULT NULL,
   `created_at` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -90,8 +153,39 @@ CREATE TABLE `students` (
 
 LOCK TABLES `students` WRITE;
 /*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (1,'Massimo111111','massimo@massimo.be','00000',102,'2020-04-26',NULL),(2,'Jan','jan@janssens.be','01234',50,NULL,NULL),(3,'Piet','piet@peeters.be','43212',20,NULL,NULL);
+INSERT INTO `students` VALUES (1,'Massimo','massimo@massimo.be','00000',102,'2020-05-01','2020-05-01'),(2,'Jan','jan@janssens.be','01234',50,'2020-05-01','2020-05-01'),(3,'Piet','piet@peeters.be','43212',20,'2020-05-01','2020-05-01'),(11,'Harry','harry@potter.com','0000000000',3,'2020-05-01','2020-05-01'),(12,'Henk','Henk@comboy.be','0000000000',12,'2020-05-01','2020-05-01'),(13,'Pinky','pinky@pie.com','0912023',12,'2020-05-02','2020-05-02'),(14,'Test','test@test.com','000000',2,'2020-05-02','2020-05-02'),(21,'Mark','Mark@vanranst.be','019202901920',54,'2020-05-04','2020-05-04');
 /*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'De Nittis Massimo','massimodn77@gmail.com',NULL,'$2y$10$s70ecVDMyIdb64ZT79Iyae87xxRL.dUsLa/oE3ST5zfHT82Sk6xem',NULL,'2020-05-03 10:54:42','2020-05-03 10:54:42');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -107,4 +201,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-27  8:12:59
+-- Dump completed on 2020-05-04 21:40:35
